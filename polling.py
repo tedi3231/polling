6c2f2e08b54a21da15e5fba63765d83118e1ac08
tedi3231 <tedi3231@gmail.com>
2014-08-19 15:37:43 +0800

    add except_type to collect record model

--- polling.py
+++ polling.py
@@ -618,6 +618,7 @@ class polling_asset_collect_record(osv.osv):
         'collect_value':fields.char(string='Collect value',size=100),
         'collect_time':fields.datetime(string='Collect time'),
         'state':fields.selection([('normal','Normal'),('lost','Lost'),('over','Over')],string='Status'),
+        'excep_type':fields.selection([('warning',"Warning"),('error','Error'),('duanxian','DuaniXian')],string='Exception Type'),
     }
 polling_asset_collect_record()
                               
