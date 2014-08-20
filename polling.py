8b0b376e465db2dd286b9e7a0e7df16a1535694c
tedi3231 <tedi3231@gmail.com>
2014-08-20 13:13:07 +0800

    add normal state to collect record

--- polling.py
+++ polling.py
@@ -618,7 +618,7 @@ class polling_asset_collect_record(osv.osv):
         'collect_value':fields.char(string='Collect value',size=100),
         'collect_time':fields.datetime(string='Collect time'),
         'state':fields.selection([('normal','Normal'),('lost','Lost'),('over','Over')],string='Status'),
-        'excep_type':fields.selection([('warning',"Warning"),('error','Error'),('duanxian','DuaniXian')],string='Exception Type'),
+        'excep_type':fields.selection([('warning',"Warning"),('error','Error'),('duanxian','DuaniXian'),('normal','Normal')],string='Exception Type'),
     }
 polling_asset_collect_record()
                               
